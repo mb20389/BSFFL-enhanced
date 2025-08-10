da0136d0ac4391c3554c2407c1d0e98daedce00d
mb20389 <matthew.w.burrill@gmail.com>
2025-08-10 08:39:05 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import "../styles/tables.css";
+import "../styles/globals.css"; // put your table styles in here
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
